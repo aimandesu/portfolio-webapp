6c95a9483898c794576472b335349c11617751fb
aimandesu <aimanafiqbinesam@gmail.com>
2023-09-22 21:09:02 +0800

    design a bit content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,7 +1,17 @@
+import classes from "./Content.module.css";
+
 const Content = (props) => {
+  //props here should terima class or some thing like that
   return (
     <>
-      <h1>content</h1>
+      <div className={classes.container}>
+        <div className={`${classes.container_item} ${classes.item_one}`}>
+          <h1>Project Name</h1>
+        </div>
+        <div className={`${classes.container_item} ${classes.item_two}`}>
+          <h1>Some Description and well supposedly leh preview</h1>
+        </div>
+      </div>
     </>
   );
 };
