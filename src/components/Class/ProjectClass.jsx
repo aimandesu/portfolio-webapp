9558b27efbcac80bd032d862c2dbd9e04760eb99
aimandesu <aimanafiqbinesam@gmail.com>
2023-09-24 11:16:36 +0800

    added picture part and the class

---
+++ src/components/Class/ProjectClass.jsx
@@ -0,0 +1,7 @@
+export default class ProjectClass {
+  constructor(name, project, photos = []) {
+    this.name = name;
+    this.project = project;
+    this.photos = photos;
+  }
+}
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,16 +1,38 @@
 import classes from "./Project.module.css";
+import ProjectClass from "../Class/ProjectClass";
 
 const Project = () => {
+  //put all our project logic in map or array?
+
+  const projects = [
+    new ProjectClass("natural hazard assistance", "description", [
+      "./Images/Mobile/1.jpg",
+      "./Images/chibi1.png",
+      "./Images/chibi1.png",
+    ]),
+    new ProjectClass("family", "description", [
+      "./Images/chibi1.png",
+      "./Images/chibi1.png",
+    ]),
+  ];
+
   return (
     <>
-      <div className={classes.container}>
-        <div className={`${classes.container_item} ${classes.item_one}`}>
-          <h1>Project Name</h1>
+      {projects.map((project, index) => (
+        <div className={classes.container} key={index}>
+          <div className={`${classes.container_item} ${classes.item_one}`}>
+            <h1>{project.name}</h1>
+            <h2>{project.project}</h2>
+          </div>
+          <div className={`${classes.container_item} ${classes.item_two}`}>
+            {project.photos.map((photo, photoIndex) => (
+              <div key={photoIndex} className={classes.part_images}>
+                <img src={photo} alt={photoIndex} />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={`${classes.container_item} ${classes.item_two}`}>
-          <h1>Some Description and well supposedly leh preview</h1>
-        </div>
-      </div>
+      ))}
     </>
   );
 };
